25ec52476d6ae7887acbb01eb6e4af02866fb43a
Peter Leung <ys.leung.uk@gmail.com>
2018-09-10 19:37:54 +0800

    Added a background UIView in the detail view controller.

--- ByeStoryboard/DetailViewController.swift
+++ ByeStoryboard/DetailViewController.swift
@@ -9,8 +9,25 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupLayout()
+    }
+    
+    //MARK: Setup the layout of the UI elements
+    func setupLayout() {
+        
+        //Create the background view
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = .white
+        view.addSubview(backgroundView)
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        backgroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 }
